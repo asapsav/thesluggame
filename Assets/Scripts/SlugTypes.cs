4d5b869566cbfda4d3c5e419270e8c159cf03085
VinDEV99 <adelucchi1999@yahoo.com>
2023-04-30 11:36:31 -0700

    Initial Unity Commit
    INFO: This should work on mac but if not let me know, I think I know how to fix it. You may also need to download a different version of Unity in order to work with the project.
    
    -Currently there is a base slug which wanders around (it also drops proto-morcells but that is for functionality purposes).
    -The ant will go to locations that the player right clicks on.

---
+++ Assets/Scripts/SlugTypes.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Slug", menuName = "Slug/New Slug")]
+public class SlugTypes : ScriptableObject
+{
+    public string slugType;
+    public int health;
+    public bool isArmored;
+}
